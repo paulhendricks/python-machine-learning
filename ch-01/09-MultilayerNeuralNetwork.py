cfbb4929afb882e535482621e7bfdf6894b20553
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-01-21 16:18:06 -0500

    Added multilayer neural network.

---
+++ ch-01/09-MultilayerNeuralNetwork.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0, 1, 1, 0]]).T
+eta = 0.1
+
+synapse_0 = 2 * np.random.random((2, 10)) - 1
+synapse_1 = 2 * np.random.random((10, 10)) - 1
+synapse_2 = 2 * np.random.random((10, 1)) - 1
+
+for _ in range(10000):
+    layer_0 = X
+    layer_1 = 1 / (1 + np.exp(-np.dot(layer_0, synapse_0)))
+    layer_2 = 1 / (1 + np.exp(-np.dot(layer_1, synapse_1)))
+    layer_3 = 1 / (1 + np.exp(-np.dot(layer_2, synapse_2)))
+    layer_3_delta = (layer_3 * (1 - layer_3)) * (y - layer_3)
+    layer_2_delta = (layer_2 * (1 - layer_2)) * np.dot(layer_3_delta, synapse_2.T)
+    layer_1_delta = (layer_1 * (1 - layer_1)) * np.dot(layer_2_delta, synapse_1.T)
+    synapse_2 += np.dot(layer_2.T, layer_3_delta)
+    synapse_1 += np.dot(layer_1.T, layer_2_delta)
+    synapse_0 += np.dot(layer_0.T, layer_1_delta)
