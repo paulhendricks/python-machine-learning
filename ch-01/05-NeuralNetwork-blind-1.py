9fa2c9e891151a8f592b37363f466936a75bb911
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-01-21 15:56:43 -0500

    First blind attempt at building a neural network to solve the xor problem. Needs to use a logistic activation function and a learning parameter where eta = (layer_n * (1 - layer_n)) and the synapses are uniformally distributed with mean of 1.

---
+++ ch-01/05-NeuralNetwork-blind-1.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0, 1, 1, 0]]).T
+eta = 0.1
+
+synapse_0 = 2 * np.random.random((2, 10)) - 1
+synapse_1 = 2 * np.random.random((10, 1)) - 1
+
+for _ in range(10000):
+    layer_0 = X
+    layer_1 = 1 / (1 + np.exp(-np.dot(layer_0, synapse_0)))
+    layer_2 = 1 / (1 + np.exp(-np.dot(layer_1, synapse_1)))
+    layer_2_delta = (layer_2 * (1 - layer_2)) * (y - layer_2)
+    layer_1_delta = (layer_1 * (1 - layer_1)) * layer_2_delta.dot(synapse_1.T)
+    synapse_1 += layer_1.T.dot(layer_2_delta)
+    synapse_0 += X.T.dot(layer_1_delta)
